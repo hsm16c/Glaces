844b8e6f3622d3c3e643184893b44126df927548
hsm16c <chahidihossam0@gmail.com>
2025-12-08 10:09:17 +0100

    Initial Commit

---
+++ glaces/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  glaces
+//
+//  Created by CHAHIDI on 08/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ glaces/glacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  glacesApp.swift
+//  glaces
+//
+//  Created by CHAHIDI on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct glacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
