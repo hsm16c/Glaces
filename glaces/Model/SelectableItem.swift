3c1b49614c0a1072862fed456961a678678be30a
hsm16c <chahidihossam0@gmail.com>
2025-12-15 12:29:55 +0100

    final: ajout de la vue de checklist et adaptation de la logique de commande
    - Ajout de la vue "Check all items" pour afficher l’ensemble des éléments du stock
    - Mise à jour de la logique pour prendre en compte les extras et le changement de contenant
    - Adaptation du flux de commande afin de préparer l’envoi par mail (fonctionnalité non finalisée)

---
+++ glaces/Model/SelectableItem.swift
@@ -0,0 +1,8 @@
+//
+//  SelectableItem.swift
+//  glaces
+//
+//  Created by CHAHIDI on 15/12/2025.
+//
+
+import Foundation
---
+++ glaces/Views/CheckALLItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckALLItemsView.swift
+//  glaces
+//
+//  Created by CHAHIDI on 15/12/2025.
+//
+
+import SwiftUI
+
+struct CheckALLItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckALLItemsView()
+}
